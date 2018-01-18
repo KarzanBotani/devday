405b1ce208bba155dceb6fcb377e47068bbdb49d
botanki <karzan@botani.nu>
2018-01-18 10:32:28 +0100

    Instagram feed now transitions in.

--- public/js/instatry.js
+++ public/js/instatry.js
@@ -1,32 +1,31 @@
 $(document).ready(function () {
-
-	var feed = new Instafeed({
-		get: 'tagged',
-		tagName: 'utvecklardag',
-		clientId: '2312486313',
-		accessToken: '2312486313.ba4c844.9b99a27fdc5a4ee2b6002c9cf5e57c44',
-		resolution: 'standard_resolution',
-		sortBy: 'most-recent',
-		template: '<li class="insta-li"><img src="{{image}}"/></li>',
-	});
-
-	feed.run();
-
-	$(window).bind('load', function () {
-		$('#instafeed').lightSlider({
-			auto: true,
-			loop: true,
-			pauseOnHover: false,
-			item: 1,
-			speed: 2000,
-			pause: 5000,
-			enableDrag: false,
-			enableTouch: false,
-		});
-	});
-
+	setTimeout(function () {
+		$('.insta-li').removeClass('hidden');
+		$('.insta-view').animate({ opacity: 1 }, 700);
+	}, 1100);
 });
 
-// accessToken: 2312486313.ba4c844.9b99a27fdc5a4ee2b6002c9cf5e57c44
-// userId: 2312486313
-// site: http://instagramwordpress.rafsegat.com/docs/get-access-token
+var feed = new Instafeed({
+	get: 'tagged',
+	tagName: 'utvecklardag',
+	clientId: '2312486313',
+	accessToken: '2312486313.ba4c844.9b99a27fdc5a4ee2b6002c9cf5e57c44',
+	resolution: 'standard_resolution',
+	sortBy: 'most-recent',
+	template: '<li class="insta-li hidden"><img src="{{image}}"/></li>',
+});
+
+feed.run();
+
+$(window).bind('load', function () {
+	$('#instafeed').lightSlider({
+		auto: true,
+		loop: true,
+		pauseOnHover: false,
+		item: 1,
+		speed: 2000,
+		pause: 5000,
+		enableDrag: false,
+		enableTouch: false,
+	});
+});
